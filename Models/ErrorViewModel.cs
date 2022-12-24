3021c6c4bbf76cc57b2a82ec2fa9f5fd4326e15c
amin-nna <aaminnnaa@gmail.com>
2022-12-24 04:12:02 -0500

    initialisation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace realEstateWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
